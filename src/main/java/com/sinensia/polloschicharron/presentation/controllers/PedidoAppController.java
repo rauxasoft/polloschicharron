5d07f59c78909fb233395b1a951b1e6a6a7908c4
jordi <jordi@gmail.com>
2025-01-24 09:32:48 +0100

    listos para practica estadisticas

--- src/main/java/com/sinensia/polloschicharron/presentation/controllers/PedidoAppController.java
+++ src/main/java/com/sinensia/polloschicharron/presentation/controllers/PedidoAppController.java
@@ -3,6 +3,7 @@ package com.sinensia.polloschicharron.presentation.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sinensia.polloschicharron.business.services.PedidoServices;
@@ -23,5 +24,13 @@ public class PedidoAppController {
 		mav.setViewName("listado-pedidos");
 		return mav;
 	}
+	
+	@GetMapping("/pedido")
+	public ModelAndView getPedido(ModelAndView mav, @RequestParam Long id) {
+		mav.addObject("pedido", pedidoServices.read(id).get());
+		mav.setViewName("pedido");
+		return mav;
+	}
+	
 	
 }
