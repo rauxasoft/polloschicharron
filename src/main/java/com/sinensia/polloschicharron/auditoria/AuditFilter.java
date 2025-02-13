9779fbab4edebcf9d39800b434330c12af2ac6f1
MANUEL CERRO TORRES <6003304@viewnext.com>
2025-02-13 14:47:03 +0100

    Cambio mínimo en AuditFilter

--- src/main/java/com/sinensia/polloschicharron/auditoria/AuditFilter.java
+++ src/main/java/com/sinensia/polloschicharron/auditoria/AuditFilter.java
@@ -3,7 +3,6 @@ package com.sinensia.polloschicharron.auditoria;
 import java.io.IOException;
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.sinensia.polloschicharron.auditoria.integration.model.RequestLogPL;
